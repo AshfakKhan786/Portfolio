37f73a5a7341eb871f6a981a9bf20d65b01b9d58
AshfakKhan786 <153702753+AshfakKhan786@users.noreply.github.com>
2025-10-20 15:46:07 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/AshfakKhan786",
   resume:
-    "https://drive.google.com/file/d/1xFnmDDuuJp1qRTzl6F2AlDdteADVKDtJ/view",
+    "https://drive.google.com/file/d/1xFnmDDuuJp1qRTzl6F2AlDdteADVKDtJ/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/ashfak-khan-he-him-2a643728b/",
   insta: "https://www.instagram.com/_ashfak04/",
 };
@@ -301,4 +301,5 @@ export const projects = [
     category: "web app",
   },
 ]
+
 
